24ef4187c417354565820630a940685206d9e92a
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2025-03-26 18:42:40 +0000

    Added one menu choice to test method calls properly work. Successfull.

--- src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
+++ src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.librarymanagementsystem;
 
+import java.util.Scanner;
+
 /**
  *
  * @author francescosciabbarrasi
@@ -12,7 +14,26 @@ public class LibraryManagementSystem {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
         DatabaseConnection dbConnection = DatabaseConnection.getInstance();
         BookManager bookManager = new BookManager();
+        
+        while (true) {
+            System.out.println("1 = Test book");
+            int choice = scanner.nextInt();
+            if (choice == -1) break;
+            
+            switch (choice) {
+                case 1: 
+                    System.out.println("Enter book title, author, genre:");
+                    scanner.nextLine();
+                    String title = scanner.nextLine();
+                    String author = scanner.nextLine();
+                    String genre = scanner.nextLine();
+                    Book bookTest = new Book(0, title, author, genre, true);
+                    bookManager.addBook(bookTest);
+            }
+        }
+        scanner.close();
     }
 }
