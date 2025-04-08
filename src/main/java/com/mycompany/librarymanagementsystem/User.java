199a9b3df64843097f75738bee5d469cb5d94a4a
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2025-04-08 10:02:07 +0100

    Added User system following factory pattern

---
+++ src/main/java/com/mycompany/librarymanagementsystem/User.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.librarymanagementsystem;
+
+/**
+ *
+ * @author francescosciabbarrasi
+ */
+public class User {
+    private int id;
+    private String name;
+    private String role; // e.g., "admin", "Member"
+    private String email;
+    private String password;
+
+    public User (int id, String name, String role, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/com/mycompany/librarymanagementsystem/UserFactory.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.librarymanagementsystem;
+
+/**
+ *
+ * @author francescosciabbarrasi
+ */
+public class UserFactory {
+    public static User createUser(int id, String name, String role, String email, String password) {
+        return new User(id, name, role, email, password);
+    } 
+}
